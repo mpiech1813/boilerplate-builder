03584278de0beec1e5227995ca9c07c37ab21f32
Maciej Pie <maciejpiech18@gmail.com>
2021-05-22 16:04:47 -0500

    index.js and init are added

---
+++ server/index.js
@@ -0,0 +1,14 @@
+const app = require('./server');
+const PORT = process.env.PORT || 3000;
+
+const init = async () => {
+  try {
+    if (!module.parent) {
+      app.listen(PORT, () => console.log(`eh, its listening I guess, ${PORT}`));
+    }
+  } catch (error) {
+    console.log('Error occured in init', error);
+  }
+};
+
+init();
