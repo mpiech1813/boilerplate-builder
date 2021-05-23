25945ddfb3e245d1ca3a8f925a1a3c5eb36a0137
Maciej Pie <maciejpiech18@gmail.com>
2021-05-23 10:57:20 -0500

    added syncAndSeed to init()

--- server/index.js
+++ server/index.js
@@ -1,8 +1,10 @@
 const app = require('./server');
+const syncAndSeed = require('./db/model/sync/syncAdnSeed');
 const PORT = process.env.PORT || 3000;
 
 const init = async () => {
   try {
+    await syncAndSeed();
     if (!module.parent) {
       app.listen(PORT, () => console.log(`eh, its listening I guess, ${PORT}`));
     }
@@ -12,5 +14,3 @@ const init = async () => {
 };
 
 init();
-
-// hello
